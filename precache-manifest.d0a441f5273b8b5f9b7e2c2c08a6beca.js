874ac066391a2ea5e6774f09db008e92fcd32d54
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-18 16:20:59 +0800

    Updates

---
+++ precache-manifest.d0a441f5273b8b5f9b7e2c2c08a6beca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45f7f89b75b69b6d894811feabd0b75",
+    "url": "/react-coin-flip/index.html"
+  },
+  {
+    "revision": "ad324c1f907d45729d77",
+    "url": "/react-coin-flip/static/css/main.b66bf942.chunk.css"
+  },
+  {
+    "revision": "24b467faaba6a99ce2e3",
+    "url": "/react-coin-flip/static/js/2.b04e1411.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-coin-flip/static/js/2.b04e1411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad324c1f907d45729d77",
+    "url": "/react-coin-flip/static/js/main.9aa2ce9f.chunk.js"
+  },
+  {
+    "revision": "d14fd31d0f356348d5e7",
+    "url": "/react-coin-flip/static/js/runtime-main.8fd5990c.js"
+  },
+  {
+    "revision": "99a107299db97ffe8a1491d5427ba596",
+    "url": "/react-coin-flip/static/media/heads.99a10729.jpg"
+  },
+  {
+    "revision": "43f803d7f5b9706cade56fa3733a8e39",
+    "url": "/react-coin-flip/static/media/tails.43f803d7.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-coin-flip/precache-manifest.d0a441f5273b8b5f9b7e2c2c08a6beca.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-coin-flip/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
